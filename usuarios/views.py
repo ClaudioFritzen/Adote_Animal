396a8c3d5c7b2ef852ed4b5bab164e434a6e1e4e
ClaudioFritzen <sdfritzen96@gmail.com>
2023-01-10 22:46:20 +0000

    criamos duas valições se email ou usuario ja existente

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
- 
+import re
 from django.contrib.auth.models import User  
 from django.contrib import messages
 from django.contrib.messages import constants
@@ -19,22 +19,52 @@ def cadastro(request):
         senha = request.POST.get('senha')
         confirmar_senha = request.POST.get('confirmar_senha')
 
+
+        # validaçao dos campos nao podem estar vazio
         if len(nome.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0 or len(confirmar_senha.strip()) == 0:
             messages.add_message(request, constants.ERROR, 'Exitem campos vazios')
             return render(request, 'cadastro.html')
 
+        # filtrando no banco se ja existe esse email
+        usuario = User.objects.filter(username = nome)
+
+        # validação para ver se existe esse usuario no banco ja
+        if usuario.exists():
+            messages.add_message(request, constants.WARNING, 'Nome de usuario já existente')
+            return render(request, 'cadastro.html', {'email': email, 'senha': senha, 'confirmar_senha': confirmar_senha}) 
+         #return HttpResponse('Cliente já existe')
+
+        # validação para ver se existe esse email no banco ja
+        email = User.objects.filter(email = email)
+        if email.exists():
+            messages.add_message(request, constants.WARNING, 'Email já utilizado')
+            return render(request, 'cadastro.html', {'nome': nome, 'senha': senha, 'confirmar_senha': confirmar_senha}) 
+         #return HttpResponse('Cliente já existe')
+
+        # validação de email com regex
+        if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):
+            messages.add_message(request, constants.WARNING, 'Digite um emai valido')
+            return render(request, {'nome': nome, 'senha': senha, 'confirmar_senha': confirmar_senha})
+
+
+                
+
+
+
+        # senha tem que ter 5 ou mais caractes
         if len(senha.strip()) <= 5:
             messages.add_message(request, constants.ERROR, 'Senha deve ser maior 5 caracteres ou mais')
-            return render(request,'cadastro.html')
+            return render(request,'cadastro.html', {'nome': nome, 'email': email})
 
+        # senha e contra senha tem que ser iguais
         if senha != confirmar_senha:
             messages.add_message(request, constants.ERROR, 'Senhas e confirmar senha não são iguais.')
             return render(request, 'cadastro.html', {'nome': nome, 'email': email})
 
         # fazer a validação de email
         # enviar email para validação de email
-        # ver o tamanho da senha > 5
-
+        # ver o tamanho da senha > 5 feito 
+  
         try:
             user = User.objects.create_user(
                 username=nome,
