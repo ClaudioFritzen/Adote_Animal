c5fd365ad07e4d7e6a8a57af690dc20f9644d43b
ClaudioFritzen <sdfritzen96@gmail.com>
2023-01-11 11:35:04 +0000

    criamos o migrate e colocamos na area administrativa

--- divulgar/admin.py
+++ divulgar/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Raca, Tag
+from .models import Raca, Tag, Pet
 
 # Register your models here.
-admin.site.register([Raca, Tag])
+admin.site.register([Raca, Tag, Pet])
