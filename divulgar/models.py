3d83e9fe2e18eb94123b87a42318f9e21dc7d857
ClaudioFritzen <sdfritzen96@gmail.com>
2023-01-11 11:32:48 +0000

    criamos a models do pet

--- divulgar/models.py
+++ divulgar/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 class Raca(models.Model):
     raca = models.CharField(max_length=50)
@@ -13,4 +15,24 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.tag
+    
+class Pet(models.Model):
+
+    choices_status = (('P', 'Para Adoção'),
+                        ('A', 'Adotado'))
+
+    usuario = models.ForeignKey(User, on_delete= models.CASCADE)
+    foto = models.ImageField(upload_to='fotos_pets')
+    nome = models.CharField(max_length=100)
+    descricao = models.TimeField()
+    estado = models.CharField(max_length=50)
+    cidade = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=20)
+    tags = models.ManyToManyField(Tag)
+    raca = models.ForeignKey(Raca, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1, choices=choices_status)
+
+
+    def __str__(self):
+        return self.nome
     
