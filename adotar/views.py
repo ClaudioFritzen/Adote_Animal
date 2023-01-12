059e152acc80518d6cbaedcdfdbb6a2c13e8f5f4
ClaudioFritzen <sdfritzen96@gmail.com>
2023-01-12 18:42:05 +0000

    passamos as raças e o status

--- adotar/views.py
+++ adotar/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from divulgar.models import Pet
+from divulgar.models import Pet, Raca
 
 # Create your views here.
 def listar_pets(request):
     if request.method == "GET":
         pets = Pet.objects.filter(status='P')
-        return render(request, 'listar_pets.html', {'pets':pets})
+        racas = Raca.objects.all()
+        return render(request, 'listar_pets.html', {'pets':pets, 'racas': racas})
