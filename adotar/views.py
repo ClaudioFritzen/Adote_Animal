cd49299518453267ed624b4e1ca23e02c901a8df
ClaudioFritzen <sdfritzen96@gmail.com>
2023-01-12 11:37:37 +0000

    colocamos o botao funcional de logout

--- adotar/views.py
+++ adotar/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from divulgar.models import Pet
 
 # Create your views here.
 def listar_pets(request):
     if request.method == "GET":
-        return render(request, 'listar_pets.html')
+        pets = Pet.objects.filter(status='P')
+        return render(request, 'listar_pets.html', {'pets':pets})
