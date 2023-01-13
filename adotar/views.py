8a795ec7f67c947618a06cd7ef055a1b582b10fc
ClaudioFritzen <sdfritzen96@gmail.com>
2023-01-13 21:58:01 +0000

    fizemos as consultas dos filtros

--- adotar/views.py
+++ adotar/views.py
@@ -6,4 +6,16 @@ def listar_pets(request):
     if request.method == "GET":
         pets = Pet.objects.filter(status='P')
         racas = Raca.objects.all()
-        return render(request, 'listar_pets.html', {'pets':pets, 'racas': racas})
+
+        cidade = request.GET.get('cidade')
+        raca_filter = request.GET.get('raca')
+
+        # validação de pesquisar a cidade por letraas parecidas
+        if cidade:
+            pets = pets.filter(cidade__icontains=cidade)
+
+        # busca no banco pelas raças
+        if raca_filter:
+            pets = pets.filter(raca__id=raca_filter)
+            raca_filter = Raca.objects.get(id=raca_filter)
+        return render(request, 'listar_pets.html', {'pets':pets, 'racas': racas, 'cidade':cidade, 'raca_filter': raca_filter})
