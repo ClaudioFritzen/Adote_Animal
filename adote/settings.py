88b5ca93adc7e69169707175dca7cfc41c257ead
ClaudioFritzen <sdfritzen96@gmail.com>
2023-01-26 23:55:41 +0000

    criamos um novo usuario

--- adote/settings.py
+++ adote/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
-#ALLOWED_HOSTS = ['127.0.0.1']
+#ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
